a4a926e73b7e937e8f324da208615bebfe2c748f
manishraj27 <manish012raj@gmal.com>
2024-04-24 01:02:26 +0530

    Implemented Song Update

--- src/components/songForm/EditSongForm.js
+++ src/components/songForm/EditSongForm.js
@@ -1,62 +1,55 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
-import styles from "./styles.module.css";
+import styles from './styles.module.css';
 import FileInput from './../fileInput/FileInput';
 
-const SongForm = ({ onClose }) => {
-  const [data, setData] = useState({
-    name: "",
-    artist: "",
-    song: "",
-    img: "",
-  });
-
+const EditSongForm = ({ song, onClose }) => {
+  const [editedData, setEditedData] = useState({ ...song });
   const [submissionStatus, setSubmissionStatus] = useState(null);
 
   const handleChange = ({ currentTarget: input }) => {
-    setData({ ...data, [input.name]: input.value });
+    setEditedData({ ...editedData, [input.name]: input.value });
   };
 
   const handleInputState = (name, value) => {
-    setData((prev) => ({ ...prev, [name]: value }));
+    setEditedData((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const authToken = localStorage.getItem("adminAuthToken");
-      const url = "http://localhost:3001/api/songs";
-      const { data: res } = await axios.post(url, data, {
+      const authToken = localStorage.getItem('adminAuthToken');
+      const url = `http://localhost:3001/api/songs/${song._id}`;
+      const { data: res } = await axios.put(url, editedData, {
         headers: {
-          "x-auth-token": authToken,
+          'x-auth-token': authToken,
         },
       });
-
       console.log(res);
-      setSubmissionStatus("success");
+      setSubmissionStatus('success');
     } catch (error) {
-      console.log(error);
-      setSubmissionStatus("error");
+      console.error('Error updating song:', error);
+      setSubmissionStatus('error');
     } finally {
-      // Call the onClose callback after 1 second
+      // Close the edit form after a short delay to allow the submission status message to be displayed
       setTimeout(() => {
         onClose();
-      }, 1000);
+      }, 1000); // Adjust the delay time as needed
     }
   };
 
   return (
     <div>
       <div className={styles.container}>
-        <form className={styles.form} onSubmit={handleSubmit} >
-          <h1 className={styles.heading}>Song Form</h1>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <h1 className={styles.heading}>Edit Song</h1>
           <input
             type="text"
             className={styles.input}
             placeholder="Song Name"
             name="name"
             onChange={handleChange}
-            value={data.name}
+            value={editedData.name}
           />
           <input
             type="text"
@@ -64,29 +57,29 @@ const SongForm = ({ onClose }) => {
             placeholder="Artist Name"
             name="artist"
             onChange={handleChange}
-            value={data.artist}
+            value={editedData.artist}
           />
           <FileInput
             name="img"
             label="Choose Image"
             handleInputState={handleInputState}
             type="image"
-            value={data.img}
+            value={editedData.img}
           />
           <FileInput
             name="song"
             label="Choose Song"
             handleInputState={handleInputState}
             type="audio"
-            value={data.song}
+            value={editedData.song}
           />
-          <button type="submit" className={styles.submit_btn} >
-            Submit
+          <button type="submit" className={styles.submit_btn}>
+            Update
           </button>
         </form>
       </div>
       {submissionStatus === 'success' && (
-        <p className={styles.successMessage}>Form submitted successfully!</p>
+        <p className={styles.successMessage}>Song updated successfully!</p>
       )}
       {submissionStatus === 'error' && (
         <p className={styles.errorMessage}>An error occurred. Please try again later.</p>
@@ -95,4 +88,4 @@ const SongForm = ({ onClose }) => {
   );
 };
 
-export default SongForm;
+export default EditSongForm;
