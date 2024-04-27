ded7a603ee9ca6e5f0dda857f04f61f01c777c83
manishraj27 <manish012raj@gmal.com>
2024-04-27 11:44:20 +0530

    mainNavBar.css error resolved

--- src/main/MainNavBar.js
+++ src/main/MainNavBar.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './mainNavBar.css'
-import SideBarMain from '../components/sidebar/SideBarMain'
-import MainHome from './MainHome'
-import UserRegistration from '../client/UserRegistration'
+import SideBarMain from '../components/sidebar/SideBarMain';
+import MainHome from './MainHome';
+import UserRegistration from '../client/UserRegistration';
 import UserLogin from './../client/UserLogin';
 import AdminLogin from './../admin/AdminLogin';
 import About from './../client/About';
@@ -11,14 +10,19 @@ import EmailVerify from '../components/emailVerify/EmailVerify';
 import EmailVerificationSuccessfullPage from '../components/emailVerify/EmailVerificationSuccessfullPage';
 import MainErrorPage from './MainErrorPage';
 
-
-export default function MainNavBar({onAdminLogin, onUserLogin}) {
+export default function MainNavBar({ onAdminLogin, onUserLogin }) {
   return (
     <Router>
-      <div className="main-body">
+      <div style={{
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: '#6ABAD2',
+        borderRadius: '30px',
+        display: 'flex',
+      }}>
         <SideBarMain />
-        < Routes>
-        <Route path="/" element={<MainHome />} />
+        <Routes>
+          <Route path="/" element={<MainHome />} />
           <Route path="/mainhome" element={<MainHome />} />
           <Route path="/userregistration" element={<UserRegistration />} />
           <Route path="/userlogin" element={<UserLogin onUserLogin={onUserLogin} />} />
@@ -30,5 +34,5 @@ export default function MainNavBar({onAdminLogin, onUserLogin}) {
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
